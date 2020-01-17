8509b6da9fc2db5ef9134c865ca2c2dae31ae407
Alexander Jansson <Alexander_jansson_91@hotmail.com>
2020-01-17 13:27:14 +0100

    La till en kommentar

---
+++ Femkamp/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Player.swift
+//  Femkamp
+//
+//  Created by Alexander Jansson on 2020-01-16.
+//  Copyright © 2020 Alexander Jansson. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    var name : String
+     var score : Int?
+    
+    init(name: String, score: Int?) {
+        self.name=name
+        self.score=score
+    }
+
+}
--- Femkamp/ViewController.swift
+++ Femkamp/ViewController.swift
@@ -8,12 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+
+    @IBOutlet weak var PickerViewScore: UIPickerView!
+    
+    var person1 = Player(name: "alex", score: nil)
+    var person2 = Player(name: "Lisa", score: nil)
+    var person3 = Player(name: "Anders", score: nil)
+    var person4 = Player(name: "Carola", score: nil)
+    
+     private var ScoreValues = (1...4).map{$0}
+      
+    @IBOutlet weak var NameLabelOne: UILabel!
+    @IBOutlet weak var NameLabelTwo: UILabel!
+    @IBOutlet weak var NameLabelThree: UILabel!
+    @IBOutlet weak var NameLabelFour: UILabel!
+    
+    @IBOutlet weak var ScoreOne: UILabel!
+    @IBOutlet weak var ScoreTwo: UILabel!
+    @IBOutlet weak var ScoreThree: UILabel!
+    @IBOutlet weak var ScoreFour: UILabel!
+    
+    
+
+    
+    var results = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        printNames ()
+        
+       
     }
+    
+    func printNames (){
+        NameLabelOne.text =  "\(person1.name)"
+        NameLabelTwo.text =  "\(person2.name)"
+        NameLabelThree.text =  "\(person3.name)"
+        NameLabelFour.text =  "\(person4.name)"
+    }
+    func CountScore (){
+     
+      
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+         return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ScoreValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return  "\(ScoreValues[row])"
+    }
+    
+    //Pickerview för spelare 1
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+           
+           ScoreOne.text = ("\(ScoreValues[row])")
+           
+           
+       }
+
+
 
 
 }
